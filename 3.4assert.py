0e7a5fc5cb4be03ed64dce70d66c16e9d43c7bdf
dengxiaobing <657563945dxb@gmail.com>
2020-09-08 22:19:32 +0800

    整理目录

--- 3.4assert.py
+++
@@ -1,8 +0,0 @@
-my_list = ['lsgogroup']
-my_list.pop(0)
-assert len(my_list) > 0
-# AssertionError
-
-# 【例子】在进行单元测试时，可以用来在程序中置入检查点，只有条件为 True 才能让程序正常工作。
-assert 3 > 7
-# AssertionError
--- 5.6raise语句.py
+++
@@ -1,9 +0,0 @@
-# raise语句
-# Python 使用raise语句抛出一个指定的异常。
-""" 例子 """
-try:
-    raise NameError('HiThere')
-except NameError:
-    print('An exception flew by!')
-
-# An exception flew by!
